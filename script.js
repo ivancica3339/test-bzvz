3965fc9fe217e448ade9db4bc56e531154de6d22
ivancica Krsinic <ivancica3339@gmail.com>
2024-05-22 17:48:14 +0200

    chore: init

---
+++ script.js
@@ -0,0 +1,16 @@
+const boja ="zuta";
+
+switch(boja){
+    case "zuta":
+        console.log("odabrao si žutu boju");
+        break;
+    case "crvena":
+        console.log("odabrao crvenu");
+        break;
+    case "zelena":
+        console.log("odabrao zelenu");
+        break;
+    default:
+        console.log("ne znam koju si boju odabrao");
+
+}
